bbd0a931ab6131e0480ff8be3781ebedc42008de
Samuel <samuelprz2305@gmail.com>
2020-08-28 18:03:12 -0400

    Cambios pendientes para Botones 2.5

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -113,14 +113,51 @@
             <v-row>
               <v-col cols="6">
                 <div class="d-flex justify-center">
-                <v-btn
-                v-if="show===true"
-                color="primary"
-                @click="openOptions()"
-                class="d-flex justify-center"
-                >
-                  New Button +
-                </v-btn>
+  <v-row justify="center">
+    <v-dialog v-model="dialog_newButton" persistent max-width="600px">
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          color="primary"
+          dark
+          v-bind="attrs"
+          v-on="on"
+        >
+          New Button +
+        </v-btn>
+      </template>
+      <v-card>
+        <v-card-title>
+          <span class="headline">Add new button</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-col cols="12" sm="6" md="12">
+                <v-text-field label="Legal first name*" required></v-text-field>
+              </v-col>
+              <v-col cols="12" sm="6" md="12">
+                <v-text-field label="Legal middle name" hint="example of helper text only on focus"></v-text-field>
+              </v-col>
+              <v-col cols="12" sm="6" md="12">
+                <v-text-field
+                  label="Legal last name*"
+                  hint="example of persistent helper text"
+                  persistent-hint
+                  required
+                ></v-text-field>
+              </v-col>
+            </v-row>
+          </v-container>
+          <small>*Verifica antes de guardar!</small>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" text @click="dialog_newButton = false">Close</v-btn>
+          <v-btn color="blue darken-1" text @click="dialog_newButton = false">Save</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-row>
                 </div>
                 <div
                   v-for="btn in arrayBotones" 
@@ -183,25 +220,25 @@
       >
       <h1>{{this.$store.state.accesoState}}</h1>
         <v-card-text>
-    <v-dialog v-model="dialog" max-width="200px">
-      <template v-slot:activator="{ on, attrs }">
-        <v-btn
-          dark
-          v-bind="attrs"
-          v-on="on"
-        >
-         <v-icon>lock</v-icon>
-        </v-btn>
-      </template>
-      <v-card>
-        <v-card-title>
-          <span class="headline px-10">Admin</span>
-        </v-card-title>
-        <v-card-text>
-          <v-text-field v-model="clave" @keyup.enter="acceder()" outlined autofocus type="password" required></v-text-field>
-        </v-card-text>
-      </v-card>
-    </v-dialog>
+        <v-dialog v-model="dialog" max-width="200px">
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+              dark
+              v-bind="attrs"
+              v-on="on"
+            >
+            <v-icon>lock</v-icon>
+            </v-btn>
+          </template>
+          <v-card>
+            <v-card-title>
+              <span class="headline px-10">Admin</span>
+            </v-card-title>
+            <v-card-text>
+              <v-text-field v-model="clave" @keyup.enter="acceder()" outlined autofocus type="password" required></v-text-field>
+            </v-card-text>
+          </v-card>
+        </v-dialog>
         </v-card-text>
 
       </v-card>
@@ -214,6 +251,7 @@
     >
       Welcome!
     </v-snackbar>
+
 </div>
 </template>
 
@@ -249,18 +287,15 @@ export default {
     data: () => ({
       dialog: false,
       snackbar: false,
+      dialog_newButton: false,
       clave: null,
       accesoClave: "3224",
       show: null,
       arrayBotones: [] 
     }),
   methods: {
-    openOptions() {
-      console.log(this.$store.state.accesoState)
-      if (this.$store.state.accesoState===true) {
-        this.show=true
-      } else this.show=false
-      
+    openNew() {
+      this.dialog_newButton=true
     },
     async acceder() {
       if (this.clave === this.accesoClave) {
